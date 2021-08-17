c3c2274a4b892a98c77566ccaaf7cdbfca38d669
Ahmed <achau1019@gmail.com>
2021-08-17 02:27:53 +0000

    Added sql;

---
+++ 3_queries_group-by/3_largest_cohorts.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name AS cohort_name, count (*) AS student_count
+FROM students 
+JOIN cohorts ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(*) >= 18
+ORDER BY student_count;
